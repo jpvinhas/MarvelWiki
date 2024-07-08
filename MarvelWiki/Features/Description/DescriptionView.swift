d672a2fed8431a0cad8d0c5460b4f17bcc7ce12c
Pedro Furtado <pedrofurtado1.9@gmaill.com>
2024-07-08 10:51:16 -0300

    DescriptionView version 1

--- MarvelWiki/Features/Description/DescriptionView.swift
+++ MarvelWiki/Features/Description/DescriptionView.swift
@@ -9,6 +9,30 @@ import SwiftUI
 
 struct DescriptionView: View {
     var body: some View {
+        HStack{
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                HStack{
+                    Image(systemName: "arrow.backward.circle.fill")
+                        .foregroundColor(.gray)
+                        .padding(.leading, 2)
+                        .font(.largeTitle)
+                }
+            })
+            AsyncImage(url:  URL(string:  "https://cdn.marvel.com/u/prod/marvel/i/mg/3/50/59132c67b2c90/clean.jpg")){ image in
+                image.image?
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                HStack{
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.gray)
+                        .padding(.leading, 2)
+                        .font(.largeTitle)
+                }
+            })
+        }
         Text("Description View")
             .foregroundStyle(Color("mClearGray"))
 
