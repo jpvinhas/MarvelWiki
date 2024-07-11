82b0a27f1230ece384a522141ce911732b701d59
ricardosvale <ricardost3@gmail.com>
2024-07-11 09:43:30 -0300

    Fix "Modificações na CharactersView"

---
+++ MarvelWiki/Features/Characters/CharactersList.swift
@@ -0,0 +1,18 @@
+//
+//  CharactersList.swift
+//  MarvelWiki
+//
+//  Created by Ricardo Silva Vale on 11/07/24.
+//
+
+import SwiftUI
+
+struct CharactersList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharactersList()
+}
