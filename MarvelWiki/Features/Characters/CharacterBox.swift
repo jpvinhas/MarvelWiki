b6e780f350279946ee8091420e0326bb64d79a82
ricardosvale <ricardost3@gmail.com>
2024-07-11 13:45:40 -0300

    Feat "Pesquisa funcionando"

---
+++ MarvelWiki/Features/Characters/CharacterBox.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterBox.swift
+//  MarvelWiki
+//
+//  Created by Ricardo Silva Vale on 11/07/24.
+//
+
+import SwiftUI
+
+struct CharacterBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterBox()
+}
---
+++ MarvelWiki/Features/Characters/SearchCharacters.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCharacters.swift
+//  MarvelWiki
+//
+//  Created by Ricardo Silva Vale on 11/07/24.
+//
+
+import SwiftUI
+
+struct SearchCharacters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchCharacters()
+}
