79141ca5c1f6279400a8ba3664086b43a603c81f
ricardosvale <ricardost3@gmail.com>
2024-07-17 11:01:54 -0300

    Feat "ShimmerEffectBox"

---
+++ MarvelWiki/Utils/Components/ShimmerEffectBox.swift
@@ -0,0 +1,18 @@
+//
+//  ShimmerEffectBox.swift
+//  MarvelWiki
+//
+//  Created by Ricardo Silva Vale on 17/07/24.
+//
+
+import SwiftUI
+
+struct ShimmerEffectBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShimmerEffectBox()
+}
